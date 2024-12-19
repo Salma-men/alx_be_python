4356f2d2924779c0f7461638ad33e4b99b42fb89
Salma Mennani <men.salmaa@gmail.com>
2024-12-19 03:27:02 +0100

    Correctly implemented arithmetic operations with print statements

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+# Assigning values to number1 and number2
+number1 = 10
+number2 = 5
+
+# Performing arithmetic operations with print statements
+
+# Addition
+print("Addition of", number1, "and", number2, "is", number1 + number2)
+
+# Subtraction
+print("Subtraction of", number1, "and", number2, "is", number1 - number2)
+
+# Multiplication
+print("Multiplication of", number1, "and", number2, "is", number1 * number2)
